633634467d2dfa4063052d9e9a52060c61b2372e
Tomohiro-Kobayashi <tkobayashi.200@gmail.com>
2023-02-25 13:17:06 +0900

    sample

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,14 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, ImageList, ImageListItem } from "@mui/material";
 import React from "react";
-import Image from "../assets/slide.png"
-import { experimentalStyled as styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-
+import Image from "../assets/slide.png";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
 
 const Item = styled(Button)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
-  padding: theme.spacing(12),
-  textAlign: 'center',
+  padding: theme.spacing(10),
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
@@ -29,18 +28,24 @@ const HomePage = () => {
           backgroundSize: "cover",
           backgroundImage: `url(${Image})`,
         }}
+      ></Box>
+      <Box
+        sx={{ marginLeft: "20px", marginRight: "20px", marginBottom: "20px" }}
       >
-      </Box>
-      <Box sx={{  marginLeft: "20px", marginRight: "20px", marginBottom: "20px"}}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {Array.from(Array(4)).map((_, index) => (
-          <Grid item xs={2} sm={4} md={4} key={index}>
-            <Item>コンテンツ</Item>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+        <ImageList
+          sx={{
+            gridTemplateColumns:
+              "repeat(auto-fill, minmax(300px, 1fr))!important",
+          }}
+        >
+          {Array.from(Array(4)).map((_, index) => (
+            <ImageListItem key={index}>
+              <Item>コンテンツ</Item>
+            </ImageListItem>
+          ))}
+        </ImageList>
 
+      </Box>
     </>
   );
 };
